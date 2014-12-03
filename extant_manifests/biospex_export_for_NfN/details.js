fe9d94c8d0f4bec1ce03b99228a0daa80a7df150
Ben W. Brumfield <benwbrum@gmail.com>
2014-12-03 14:07:04 -0600

    Initial check-in of manifests repo.

---
+++ extant_manifests/biospex_export_for_NfN/details.js
@@ -0,0 +1,122 @@
+{
+    "sourceDir": "/vagrant/biospex/app/storage/data/4-479dc46cfd",
+    "targetDir": "/vagrant/biospex/app/storage/data/4-479dc46cfd",
+    "created_at": "Tuesday 2nd December 2014",
+    "highResDir": "/vagrant/biospex/app/storage/data/4-479dc46cfd/large",
+    "lowResDir": "/vagrant/biospex/app/storage/data/4-479dc46cfd/small",
+    "highResWidth": 1540,
+    "lowResWidth": 580,
+    "images": [
+        {
+            "identifier": "urn:uuid:c874b334-238c-4235-bc12-6caea16707a5",
+            "original": {
+                "path": [
+                    "546b8f8cb0e6dffe0e8b45d4",
+                    ".jpg"
+                ],
+                "name": "546b8f8cb0e6dffe0e8b45d4.jpg",
+                "width": 3657,
+                "height": 5617
+            },
+            "large": {
+                "name": "large/546b8f8cb0e6dffe0e8b45d4.large.png",
+                "width": 1540,
+                "height": 2365
+            },
+            "small": {
+                "name": "small/546b8f8cb0e6dffe0e8b45d4.small.png",
+                "width": 580,
+                "height": 891
+            }
+        },
+        {
+            "identifier": "urn:uuid:6d4a616d-cbca-4d29-83c9-8c6f6b0b421f",
+            "original": {
+                "path": [
+                    "546b8f8cb0e6dffe0e8b45d5",
+                    ".jpg"
+                ],
+                "name": "546b8f8cb0e6dffe0e8b45d5.jpg",
+                "width": 3382,
+                "height": 5425
+            },
+            "large": {
+                "name": "large/546b8f8cb0e6dffe0e8b45d5.large.png",
+                "width": 1540,
+                "height": 2470
+            },
+            "small": {
+                "name": "small/546b8f8cb0e6dffe0e8b45d5.small.png",
+                "width": 580,
+                "height": 930
+            }
+        },
+        {
+            "identifier": "urn:uuid:9d8eeca8-99e4-4e11-866d-1dbe1048ad29",
+            "original": {
+                "path": [
+                    "546b8f8cb0e6dffe0e8b45d6",
+                    ".jpg"
+                ],
+                "name": "546b8f8cb0e6dffe0e8b45d6.jpg",
+                "width": 3408,
+                "height": 5217
+            },
+            "large": {
+                "name": "large/546b8f8cb0e6dffe0e8b45d6.large.png",
+                "width": 1540,
+                "height": 2357
+            },
+            "small": {
+                "name": "small/546b8f8cb0e6dffe0e8b45d6.small.png",
+                "width": 580,
+                "height": 888
+            }
+        },
+        {
+            "identifier": "urn:uuid:12591de2-aca3-4fa0-96d5-83f2758c2498",
+            "original": {
+                "path": [
+                    "546b8f8cb0e6dffe0e8b45d7",
+                    ".jpg"
+                ],
+                "name": "546b8f8cb0e6dffe0e8b45d7.jpg",
+                "width": 3432,
+                "height": 5233
+            },
+            "large": {
+                "name": "large/546b8f8cb0e6dffe0e8b45d7.large.png",
+                "width": 1540,
+                "height": 2348
+            },
+            "small": {
+                "name": "small/546b8f8cb0e6dffe0e8b45d7.small.png",
+                "width": 580,
+                "height": 884
+            }
+        },
+        {
+            "identifier": "urn:uuid:e4480e20-b700-4401-80f1-e057b599f12a",
+            "original": {
+                "path": [
+                    "546b8f8cb0e6dffe0e8b45d8",
+                    ".jpg"
+                ],
+                "name": "546b8f8cb0e6dffe0e8b45d8.jpg",
+                "width": 3466,
+                "height": 5525
+            },
+            "large": {
+                "name": "large/546b8f8cb0e6dffe0e8b45d8.large.png",
+                "width": 1540,
+                "height": 2455
+            },
+            "small": {
+                "name": "small/546b8f8cb0e6dffe0e8b45d8.small.png",
+                "width": 580,
+                "height": 925
+            }
+        }
+    ],
+    "total": 10
+}
